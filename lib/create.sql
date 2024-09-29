c7b4d3b2156c9423fa21ade35924bbb78d50f7e8
Kevin <kvilleda10566@gmail.com>
2024-09-29 19:12:00 -0400

    Done

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,     -- Unique identifier for each bear
+  name TEXT,                  -- Bear's name
+  age INTEGER,                -- Bear's age
+  sex TEXT,                   -- Bear's sex (male/female)
+  color TEXT,                 -- Color of the bear
+  temperament TEXT,           -- Bear's temperament
+  alive BOOLEAN               -- Whether the bear is alive or not (true/false)
+);
---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color,temperament,alive)
+VALUES('Mr. Chocolate', 8, 'M','Brown','mad',TRUE);
+INSERT INTO bears (name, age, sex, color,temperament,alive)
+VALUES('Rowdy', 1, 'M','Brown','curious',TRUE);
+INSERT INTO bears (name, age, sex, color,temperament,alive)
+VALUES('Tabitha', 2, 'F','Brown','calm',TRUE);
+INSERT INTO bears (name, age, sex, color,temperament,alive)
+VALUES('Sergeant Brown', 4, 'M','Brown','friendly',FALSE);
+INSERT INTO bears (name, age, sex, color,temperament,alive)
+VALUES('Melissa', 5, 'F','Brown','grump',TRUE);
+INSERT INTO bears (name, age, sex, color,temperament,alive)
+VALUES('Grinch', 3, 'M','Brown','playful',FALSE);
+INSERT INTO bears (name, age, sex, color,temperament,alive)
+VALUES('Wendy', 6, 'F','Brown','aggressive',TRUE);
+INSERT INTO bears (name, age, sex, color,temperament,alive)
+VALUES(NULL, 7, 'F','Brown','mysterious',TRUE);
